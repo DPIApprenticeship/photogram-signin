ec636978df2a3b33dbc001725cbe0a7b8bdc37f2
mpuchyr <matt.puchyr@gmail.com>
2022-10-24 16:27:32 +0000

    Add functionality to sign in and sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,10 +28,40 @@ class UsersController < ApplicationController
 
     if save_status == true
 
+      session.store(:user_id, user.id)
       redirect_to("/users/#{user.username}", {:notice => "Welcome, #{user.username}"})
     else
 
       redirect_to("/user_sign_up", {:alert => user.errors.full_messages.to_sentence})
+    end
+  end
+
+  def user_sign_out
+    reset_session
+
+    redirect_to("/", {:notice => "See ya later"})
+  end
+
+  def new_session_form
+    render({ :template => "users/signin_form.html.erb"})
+  end
+
+  def authenticate
+    un = params.fetch(:input_username)
+    pw = params.fetch(:input_password)
+
+    user = User.where({ :username => un}).at(0)
+
+    if user == nil
+      redirect_to("/user_sign_in", {:alert => "No one by that name here"})
+    else
+      if user.authenticate(pw)
+        session.store(:user_id, user.id)
+        redirect_to("/", :notice => "Welcome back, #{user.username}")
+      else
+
+        redirect_to("/user_sign_in", :alert => "Nice try")
+      end
     end
   end
 
